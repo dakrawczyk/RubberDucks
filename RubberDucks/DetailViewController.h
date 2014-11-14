289fe94981a0185c7bf68743bc2f7f349779fc6c
Dave Krawczyk <dave.krawczyk@gmail.com>
2014-11-14 10:45:13 -0800

    ready for demo

---
+++ RubberDucks/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  RubberDucks
+//
+//  Created by Dave Krawczyk on 11/14/14.
+//  Copyright (c) 2014 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
---
+++ RubberDucks/DetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  DetailViewController.m
+//  RubberDucks
+//
+//  Created by Dave Krawczyk on 11/14/14.
+//  Copyright (c) 2014 MobileMakers. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *duckNameLabel;
+
+@end
+
+@implementation DetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
